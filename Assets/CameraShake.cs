fbbff041e27f5bf3c8f3ebe871edfd1be7ce92b1
djedr <jdwork@o2.pl>
2015-05-19 22:38:21 +0200

    merged interface changes with new stuff

---
+++ Assets/CameraShake.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraShake : MonoBehaviour {
+	public float shake = 0;
+	public float shakeAmount = 0.1f;
+	public float decreaseFactor = 1;
+
+	private Vector3 initialPosition;
+	private bool shakeStarted = false;
+	
+	// Use this for initialization
+	void Start () {
+		initialPosition = this.transform.localPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (shake > 0) {
+			if (shakeStarted == false) {
+				initialPosition = this.transform.localPosition;
+				shakeStarted = true;
+			}
+
+			this.transform.localPosition = initialPosition + Random.insideUnitSphere * shakeAmount;
+			shake -= Time.deltaTime * decreaseFactor;
+		} else {
+			shake = 0;
+			this.transform.localPosition = initialPosition;
+			shakeStarted = false;
+		}
+	}
+}
--- Assets/UIScript.cs
+++ Assets/UIScript.cs
@@ -5,7 +5,16 @@ using System.Collections;
 public class UIScript : MonoBehaviour {
 
 	public Text text;
+	public Text autopilotText;
+	public Light emergencyLight;
+	public Light autopilotLight;
+	public GameObject camera;
 	public PlayerScript playerScript;
+	public float emergencyTimeout = 0.8f;
+	public float autopilotTimeout = 0.8f;
+
+	private float currentEmergencyTimeout = 0.0f;
+	private float currentAutopilotTimeout = 0.0f;
 
 	private Color[] colors = {
 		new Color(0.9f, 0.2f, 0.185f),
@@ -27,13 +36,19 @@ public class UIScript : MonoBehaviour {
 		switch (playerScript.state) {
 		case PlayerScript.StateTypes.OnStart:
 			uiText = "PRZYGOTUJ SIĘ";
+			autopilotLight.enabled = true;
+			autopilotText.color = Color.blue;
 			break;
 		case PlayerScript.StateTypes.OnLaunch:
 			uiText = "WYŁĄCZAM AUTOPILOT";
+			autopilotLight.enabled = true;
+			autopilotText.color = Color.blue;
 			color = colors[3];
 			break;
 		case PlayerScript.StateTypes.InControl:
 			uiText = "OMIJAJ ASTEROIDY";
+			autopilotLight.enabled = false;
+			autopilotText.color = Color.black;
 			color = colors[3];
 			break;
 		case PlayerScript.StateTypes.LightSpeed:
@@ -42,18 +57,59 @@ public class UIScript : MonoBehaviour {
 			break;
 		case PlayerScript.StateTypes.BeforeLightSpeed:
 			uiText = "WŁĄCZAM NAPĘD WARP";
+			autopilotLight.enabled = true;
+			autopilotText.color = Color.blue;
 			color = colors[3];
 			break;	
 		case PlayerScript.StateTypes.Bounced:
 			uiText = "UWAGA!";
+			//autopilotLight.enabled = true;
+			//autopilotText.color = Color.blue;
 			color = colors[0];
+			if (currentEmergencyTimeout <= 0f) {
+				currentEmergencyTimeout = emergencyTimeout;
+				camera.GetComponent<CameraShake>().shake = 0.4f;
+				emergencyLight.enabled = true;
+			}
+			break;
+		case PlayerScript.StateTypes.Landing:
+			uiText = "INICJUJĘ DOKOWANIE";
+			autopilotLight.enabled = true;
+			autopilotText.color = Color.blue;
+			break;
+		case PlayerScript.StateTypes.Landed:
+			uiText = "STATEK ZADOKOWANY!";
+			autopilotLight.enabled = true;
+			autopilotText.color = Color.blue;
+			autopilotLight.color = colors[1];
 			break;
 		default:
 			uiText = playerScript.state.ToString();
 			break;
 		}
 
+//		if (currentAutopilotTimeout <= 0f) {
+//			currentAutopilotTimeout = autopilotTimeout;
+//			autopilotLight.enabled = true;
+//		}
+
+		if (autopilotLight.enabled) {
+			if (Mathf.Repeat(Time.time, 0.5f) > 0.25f) autopilotText.color = Color.blue;
+			else autopilotText.color = Color.black;
+			//if (autopilotLight.intensity <= 0f) autopilotLight.intensity = 8f;
+		}
+
 		text.text = uiText;
 		text.color = color;
+
+		if (currentEmergencyTimeout > 0f) {
+			float oldEmergencyTimeout = currentEmergencyTimeout;
+			currentEmergencyTimeout -= Time.deltaTime;
+			emergencyLight.intensity -= 1f;
+			if (emergencyLight.intensity <= 0f) emergencyLight.intensity = 8f;
+
+		} else {
+			emergencyLight.enabled = false;
+		}
 	}
 }
